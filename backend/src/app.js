d595232dfedebce33424b51535bd1b0792bc330b
fahimlisl <developerfahim134@gmail.com>
2026-01-11 16:55:53 +0530

    sh cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,7 @@ import cors from "cors"
 const app = express();
 
 app.use(cors({
-    // origin:process.env.CORS_ORIGIN,
-    origin:"http://localhost:5173",
+    origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
 
